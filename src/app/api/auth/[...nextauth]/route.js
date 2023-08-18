9ef928de9c983c1c2c4a9f13b2ba08f61d68d5a2
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-08-18 17:47:13 +0600

    Added user page successfully three

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,35 @@
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// export const authOptions = NextAuth({
+//   // Configure one or more authentication providers
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+//   secret: process.env.NEXT_AUTH_SECRET,
+// });
+
+// export { authOptions as GET, authOptions as POST };
+
+// Chat gpt code:
+
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const options = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXT_AUTH_SECRET,
+};
+
+export default (req, res) => NextAuth(req, res, options);
