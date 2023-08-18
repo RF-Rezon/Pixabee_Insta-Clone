5ce74c334d32ad8dadb3c93d63682fad8a52168d
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-08-18 17:42:29 +0600

    Added user page successfully two

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -5,8 +5,8 @@ export const authOptions = NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     // ...add more providers here
   ],
@@ -14,4 +14,3 @@ export const authOptions = NextAuth({
 });
 
 export { authOptions as GET, authOptions as POST };
-
